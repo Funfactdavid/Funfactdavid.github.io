0b6e1408d867d764672274c952850b5d018b1302
Funfactdavid <81843320+Funfactdavid@users.noreply.github.com>
2021-04-04 22:36:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
  function myfunction(){
-      document.getElementById("dabide").innerHTML = "DABIDE IS MEXICAN" ;
+      document.getElementById("dabide").innerHTML = "DABABY'S GENDER IS STEVE" ;
       var img = document.createElement('img');
 document.getElementById("pic").style.opacity = "1";
     }
